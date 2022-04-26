a610ee9ac1b19be75bfbaf5cea05a325a359602d
samir <s-malik@live.co.uk>
2022-04-26 11:43:58 +0100

    Added the setup test database method - need to look at errors

--- spec/setup_test_datebase.rb
+++ spec/setup_test_datebase.rb
@@ -1,7 +1,7 @@
 require 'pg'
 
-p "Setting up test database..."
-
-connection = PG.connect(dbname: 'chitter_2_test')
-
-connection.exec("TRUNCATE chitter_messages;")
+def setup_test_database
+    p "Setting up test database..."
+    connection = PG.connect(dbname: 'chitter_2_test')
+    connection.exec("TRUNCATE chitter_messages;")
+end     
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,14 @@
+require_relative './setup_test_database'
+
 ENV['RACK_ENV'] = 'test'
 
-require File.join(File.dirname(__FILE__),'..','app.rb')
+RSpec.configure do |config|
+  config.before(:each) do
+    setup_test_database
+  end
+end 
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 require 'capybara'
 require 'capybara/rspec'
