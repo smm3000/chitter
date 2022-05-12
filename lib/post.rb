e5471a8f574b8f00acd9c85f5652f4575ae74161
samir <s-malik@live.co.uk>
2022-05-12 14:31:33 +0100

    ID pulling through - but need to rewrite/amend tests as some failing due to parameter reqs

--- lib/post.rb
+++ lib/post.rb
@@ -1,15 +1,24 @@
 require 'pg'
 
 class Post 
+
+    attr_reader :id, :message
+
+    def initialize(id:, message:)
+        @id = id
+        @message = message
+    end 
+
     def self.all 
         if ENV['RACK_ENV'] == 'test'
             connection = PG.connect(dbname: 'chitter_2_test')
         else 
             connection = PG.connect(dbname: 'chitter_2')
         end 
-
         result = connection.exec("SELECT * FROM chitter_messages;")
-        result.map { |peep| peep['message'] }
+        result.map do |peep|
+            Post.new(id: peep['id'], message: peep['message'])
+        end 
     end 
 
     def self.create(message:)
@@ -18,6 +27,8 @@ class Post
           else
             connection = PG.connect(dbname: 'chitter_2')
           end
-        connection.exec("INSERT INTO chitter_messages (message) VALUES('#{message}')")
+
+        result = connection.exec("INSERT INTO chitter_messages (message) VALUES('#{message}') RETURNING id, message;")
+        Post.new(id: result[0]['id'], message: result[0]['message'])
     end 
 end 
